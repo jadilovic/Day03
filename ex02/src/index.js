5a0569b3a9170d34a30c6887434e07bb31ac4a7a
jadilovic <adilovic79@yahoo.com>
2021-03-10 13:20:02 +0100

    Exercise 2 saved file commited

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    // Only change code below this line 
+
+    // This is one line comment
+
+    /* This is mulit-line comment
+    This is multi line comment
+    This is multi line comment
+    */
+
+    // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
